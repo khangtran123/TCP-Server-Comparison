af36d5160d1536d89ab9b551f3ed460b44ab149e
khangtran123 <khangh.tran@hotmail.com>
2018-02-24 00:56:49 -0800

    Update client.py

--- client.py
+++ client.py
@@ -1,3 +1,5 @@
+#!/bin/python2
+
 '''
 File: client.py
 Date: February 18, 2018
@@ -8,12 +10,23 @@ Description: This scripts covers the general client that will spawn a bunch of
              epoll servers. 
 '''
 
+import threading
 import socket
 import sys
 import time
+import os
+from signal import signal, SIGPIPE, SIG_DFL
 
+global output_file
+global clients
+global echoedNUM
+global RTT
+global totalMessages
+#  totalRequests - How many requests meaning connections and echoes included towards the server
+global totalRequests
+totalMessages = 0
 
-# THis function invokes multi-threading to create multiple clients
+#  THis function invokes multi-threading to create multiple clients
 def spawn_clients(machineAddr, clients, serverAddr, serverPort, echoedMsg, echoedNUM):
     threadQueue = []
     clientID = 1
@@ -22,7 +35,7 @@ def spawn_clients(machineAddr, clients, serverAddr, serverPort, echoedMsg, echoe
         thread = threading.Thread(target=start_engine, args=(machineAddr, clientID, serverAddr, serverPort, echoedMsg, echoedNUM))
         #  now we want to load up the array "threadQueue"
         threadQueue.append(thread)
-        print ("Starting Client #" + str(threadID))
+        print ("Starting Client #" + str(clientID))
         thread.start()
         clientID += 1
 
@@ -31,18 +44,18 @@ def spawn_clients(machineAddr, clients, serverAddr, serverPort, echoedMsg, echoe
         thread.join()
 
 def start_engine(machineAddr, clientID, serverAddr, serverPort, echoedMsg, echoedNUM):
-    global timeRoundtrip
-    global finalMsgCount
-    
+
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.connect((serverAddr, serverPort))
-    
+
     while True:
         # this iterates through the user-specified number of echoes that will be sent out
         for i in range(echoedNUM + 1):
             # start the time once msg is sent to server
             startTime = time.time()
-            echoData = "Message: " + str(echoedMsg) + " --> From Client: " + str(clientID) + "/" + str(machineAddr)
+            #echoData = "Message: " + str(echoedMsg) + " --> From Client: " + str(clientID) + "/" + str(machineAddr)
+            echoData = "Message: " + str(echoedMsg)
+            # totalMessages = ""
             #  totalLenData = len(echoData)
             # this condition is meant for select/epoll server: once the list of messages have been iterated and sent
             # this specific client has to send a quit so that the server can move on to another client session
@@ -53,43 +66,53 @@ def start_engine(machineAddr, clientID, serverAddr, serverPort, echoedMsg, echoe
                 s.send(echoData)
                 print ("Message sent!")
                 # This value will continuously increment in value after every msg sent --> will output the total bytes sent to client
-                finalMsgCount += echoData
+                totalMessages += echoData
             recvMsg = s.recv(1024)
-            print ("Received Msg: " + str(recvMsg))
+            print ("Received Msg: " + str(recvMsg) + " Bytes")
             # once we receive the echo back from server, we end the timer
             endTime = time.time()
-            # Now we gather the round trip time of the msg being sent from client to server and then from back from server to client. 
-            timeRoundtrip = endTime - startTime
-        #now we want to output the results of this echo stats to the log file
-        output_file.write("\n Client #" + str(clientID) + " sent out a total of " + str(echoedNUM) + " messages with a total roundtrip time of " + str(timeRoundtrip) + " seconds.")
-                
+            # Now we gather the round trip time of the msg being sent from client to server and then from back from server to client.
+            RTT = endTime - startTime
+            totalRTT += RTT
+            avgRTT = RTT / totalRTT
+            #  now we want to output the results of this echo stats to the log file
+            output_file.write("\n Client #" + str(clientID) + " sent out a total of " + str(echoedNUM) + " messages with a total roundtrip time of " + str(RTT) + " seconds.")
+            result_to_file(RTT, totalRTT, avgRTT, totalMessages)
             
-def result_to_file():
-    return
 
+def result_to_file(RTT, totalRTT, avgRTT, totalMessages):
+    output_file.write("\n Total Number of Clients: " + clients)
+    output_file.write("\n Total Number of Requests: " + totalRequests)
+    output_file.write("\n Total Data Sent to server: " + str(totalMessages) + " Bytes")
+    output_file.write("\n Total Roundtrip Time for each echo: " + str(totalRTT) + " seconds")
+    output_file.write("\n Average Roundtrip Time for each echo: " + str(avgRTT) + " seconds")
+    
+    
 def main():
-    global clients
-    global echoedNUM
     
     serverAddr = raw_input("Enter the server ip address: ")
     serverPort = 7005
-    
+
     #s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     #s.connect((serverIP, port))
     output_file = open('Client-Output.txt','w')
-    
+    signal(SIGPIPE, SIG_DFL)
+
     machineAddr = raw_input("What is your IP Address? (i.e. 192.168.0.X) ")
     clients = raw_input("How many clients would you like to spawn? (Enter an Integer value) ")
-    echoedMsg = raw_input("What do you want to echo to the server? (Enter a string)")
+    echoedMsg = raw_input("What do you want to echo to the server? (Enter a string) ")
     echoedNUM = raw_input("How many times do you want this message echoed from each client to the server? (Enter an integer value) ")
-    option = raw_input("Would you like to commence the echo program? (type 'begin')")
+    option = raw_input("Would you like to commence the echo program? (type 'begin') ")
     
+    totalRequests = ((clients * echoedNUM) + clients)
+
     if option == "begin":
         # we want to makes sure we're not passing a null msg to the server
         if echoedMsg == "":
             print ("The message cannot be null! You must send something")
             echoedMsg = raw_input("What do you want to echo to the server? (Enter a string)")
         else:
-            spawn_clients(machineAddr, clients, serverAddr, serverPort, echoedMsg, echoedNUM)
+            spawn_clients(machineAddr, int(clients), serverAddr, serverPort, echoedMsg, int(echoedNUM))
+
 
 if __name__ == "__main__": main()
